03d72fbfee6d72a45ba3e82313682dd02cf76f58
Matheus Henrique de Almeida Santos <103071070+matheus-hdas@users.noreply.github.com>
2024-09-30 16:41:30 +0000

    add pages folder and the index page

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <h1>Lumi, Você é foda.<br>
+            To testando servidor e lembrei de você.<br>
+            Quando ver dá uma risadinha e me responde com um Joinha.
+        </h1>
+    );    
+}
+
+export default Home;
