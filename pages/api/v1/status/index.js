b52c9ca7700b40a10903be9a0a2d2577d949f812
matheus-hdas <matheussantos.betim@gmail.com>
2024-10-05 23:26:09 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,39 @@
 import database from "infra/database.js";
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  res.status(200).json({});
+  const databaseName = process.env.POSTGRES_DB;
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionRs = await database.query(
+    "SHOW server_version;"
+  );
+  const databaseVersionValue = databaseVersionRs.rows[0].server_version;
+
+  const databaseMaxConnectionsRs = await database.query(
+    "SHOW max_connections;"
+  );
+  const databaseMaxConnectionsValue = parseInt(databaseMaxConnectionsRs.rows[0].max_connections);
+
+//  const databaseOpenConnectionsRs = await database.query(
+//    `SELECT count(*)::int FROM pg_stat_activity WHERE datname = '${databaseName}';`
+//  );
+
+  const databaseOpenConnectionsRs = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
+    values: [ databaseName ]
+  });
+  const databaseOpenConnectionsValue = databaseOpenConnectionsRs.rows[0].count;
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: databaseMaxConnectionsValue,
+        open_connections: databaseOpenConnectionsValue
+      }
+    }
+  });
 }
 
 export default status;
