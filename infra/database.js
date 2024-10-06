22f76ea49d4133e21df70744fe2d9a031940c082
matheus-hdas <matheussantos.betim@gmail.com>
2024-10-06 01:52:26 -0300

    add a more robust ssl configurantion method

--- infra/database.js
+++ infra/database.js
@@ -7,9 +7,9 @@ async function query(queryObject) {
         password: process.env.POSTGRES_PASSWORD,
         user: process.env.POSTGRES_USER,
         database: process.env.POSTGRES_DB,
-        ssl: process.env.NODE_ENV === "development" ? false : true
+        ssl: getSSLValues()
     });
-    
+
     try {
         await client.connect();
         const result = await client.query(queryObject);
@@ -20,6 +20,15 @@ async function query(queryObject) {
     } finally {
         await client.end();
     }
+}
+
+function getSSLValues() {
+    if(process.env.POSTGRES_CA) {
+        return {
+            ca: process.env.POSTGRES_CA
+        };
+    }
+    return process.env.NODE_ENV == "development" ? false : true;
 }
 
 export default {
