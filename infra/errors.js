1bfeb1dd17dc3da2e7cd438f8926beea8aa53774
matheus-hdas <matheussantos.betim@gmail.com>
2025-01-20 01:39:17 -0300

    feat: create and use `InternalServerError` error class

---
+++ infra/errors.js
@@ -0,0 +1,20 @@
+export class InternalServerError extends Error {
+  constructor({ cause }) {
+    super("An unexpected error occurred.", {
+      cause,
+    });
+
+    this.name = "InternalServerError";
+    this.action = "Please contact support.";
+    this.statusCode = 500;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
